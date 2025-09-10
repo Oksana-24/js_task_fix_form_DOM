7c15a9deae54e04695562b5fdef7c5928589b058
Oksana Rubel <okhymych480@gmail.com>
2025-09-10 16:44:39 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,32 @@
 'use strict';
 
-const inputs = document.querySelectorAll('form input');
+const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
   if (!input.id && input.name) {
     input.id = input.name;
   }
 
+  if (!input.name) {
+    return;
+  }
+
   const label = document.createElement('label');
   const formattedName = textToCapitalize(input.name);
 
-  label.className = 'field-label';
+  label.classList.add('field-label');
   label.htmlFor = input.id;
-  label.innerText = formattedName;
+  label.textContent = formattedName;
   input.placeholder = formattedName;
 
   input.before(label);
 });
 
 function textToCapitalize(text) {
+  if (!text) {
+    return '';
+  }
+
   return (
     text.charAt(0).toUpperCase() +
     text
