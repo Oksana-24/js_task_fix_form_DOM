ebae774447c59582b8733ae87471cc89a0a1ad0e
Oksana Rubel <okhymych480@gmail.com>
2025-09-10 16:27:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  if (!input.id && input.name) {
+    input.id = input.name;
+  }
+
+  const label = document.createElement('label');
+  const formattedName = textToCapitalize(input.name);
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.innerText = formattedName;
+  input.placeholder = formattedName;
+
+  input.before(label);
+});
+
+function textToCapitalize(text) {
+  return (
+    text.charAt(0).toUpperCase() +
+    text
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .toLowerCase()
+      .slice(1)
+  );
+}
